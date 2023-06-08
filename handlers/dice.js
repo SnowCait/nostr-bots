c2349719de26d6e8341c54e9ef8067823e5f615f
SnowCait <SnowCait@users.noreply.github.com>
2023-06-08 23:53:00 +0900

    Dice

---
+++ handlers/dice.js
@@ -0,0 +1,53 @@
+import { getEventHash, getPublicKey, getSignature } from 'nostr-tools';
+import 'websocket-polyfill'
+import { getSeckey } from '../libs/nostr.js';
+
+const nsecKey = 'nostr-test-bot-nsec';
+
+export const handler = async (e) => {
+  console.log('[request]', JSON.stringify(e))
+  const json = e.isBase64Encoded ? Buffer.from(e.body, 'base64').toString() : e.body
+  console.log('[request event]', json)
+
+  const requestEvent = JSON.parse(json)
+
+  const seckey = await getSeckey(nsecKey)
+  const pubkey = getPublicKey(seckey)
+
+  const match = requestEvent.content.match(/\b(?<number>[1-9]\d*)d(?<face>[1-9]\d*)\b/)
+  const number = Number(match.groups.number)
+  const face = Number(match.groups.face)
+
+  const dices = Array.from(
+    {length: number},
+    (v, i) => Math.floor(Math.random() * face)
+  )
+  const sum = dices.reduce((sum, dice) => sum + dice + 1, 0)
+  console.log('[dices]', dices, sum)
+
+  const diceStrings = [...'⚀⚁⚂⚃⚄⚅'];
+  let content = face === 6
+    ? dices.map(dice => diceStrings[dice]).join('')
+    : dices.map(dice => dice + 1).join(', ')
+  content += `\nSum: ${sum}`
+
+  const event = {
+    kind: 1,
+    created_at: Math.floor(Date.now() / 1000),
+    tags: [
+      ['e', requestEvent.id, '', 'root'],
+      ['p', requestEvent.pubkey]
+    ],
+    content,
+    pubkey
+  }
+
+  event.id = getEventHash(event)
+  event.sig = getSignature(event, seckey)
+  console.log('[event]', event)
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(event)
+  };
+};
