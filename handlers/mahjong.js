d02ff4bdf19bf9c9a7eed4adb5abfcff48551965
SnowCait <SnowCait@users.noreply.github.com>
2023-08-30 11:59:17 +0900

    Reply with same kind

--- handlers/mahjong.js
+++ handlers/mahjong.js
@@ -32,7 +32,7 @@ export const handler = async (e) => {
 
   const event = customEmojiMode
     ? {
-      kind: 1,
+      kind: requestEvent.kind,
       created_at: Math.floor(Date.now() / 1000),
       tags: [
         ['e', requestEvent.id, '', 'root'],
@@ -45,7 +45,7 @@ export const handler = async (e) => {
       pubkey
     }
     : {
-      kind: 1,
+      kind: requestEvent.kind,
       created_at: Math.floor(Date.now() / 1000),
       tags: [
         ['e', requestEvent.id, '', 'root'],
